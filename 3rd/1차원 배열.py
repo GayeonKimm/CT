5d9af0e407a2a11275ee4acabccb8ca153d809ca
y <gyk9704@gmail.com>
2021-11-07 00:21:21 +0900

    1107

---
+++ 3rd/1차원 배열.py
@@ -0,0 +1,62 @@
+# 10818 최소 최대
+# n = int(input())
+# list = list(map(int, input().split()))
+# print(max(list), min(list))
+
+# 근데 이럴거면 n은 굳이 왜 받음? 킹받네
+
+
+# 2562 최댓값
+# 인덱스값 출력하는거 뭐였더라...list.index였나?
+# list = []
+# for i in range(9):
+#     num = int(input())
+#     list.append(num)
+# print(max(list))
+# print(list.index(max(list))+1)
+
+# 2577 숫자의 개수
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# list= list(str(a*b*c))
+# # print(list)
+# # print(list.count('0'))
+# # 하면 0 의 갯수 카운트 옵션 사용
+#
+# for i in range(10):
+#     print(list.count(str(i)))
+# str 안하면 리스트 문자 인식 못해서 안된다 꼭 해주기
+
+
+# 3052 나머지
+# list1 =[]
+# for i in range(10):
+#     n = int(input())
+#     list1.append(n%42)
+# list2 = set(list1)
+# print(len(list2))
+
+# 1546 평균
+# n = int(input())
+# score = list(map(int, input().split()))
+# m = max(score)
+#
+# sum = 0
+# for i in range(n):
+#     score[i] = (score[i]/m)*100
+#     sum += score[i]
+# print(sum/n)
+
+
+# 8958 OX퀴즈
+n = int(input())
+for i in range(n):
+    score = list(input())
+    for j in range():
+
+        if s =='O':du tn sjde dls srj d;Td, s
+
+
+
+# 4344 평균은 넘겠지
