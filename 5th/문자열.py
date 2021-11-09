ec732fc9a7a4da445bd2b7f9899fa8f1e070e4cb
y <gyk9704@gmail.com>
2021-11-09 19:02:17 +0900

    1109

--- 5th/문자열.py
+++ 5th/문자열.py
@@ -12,8 +12,8 @@
 # print(num1) if num1>num2 else print(num2)
 
 # 5622 다이얼
-# word = input()
 # alphabet = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+# word = input()
 # time = 0
 #
 # for unit in alphabet:
@@ -24,10 +24,31 @@
 # print(time)
 
 # 2941 크로아티아 알파벳
-a = input()
-cro = ['c=','c-','dz=','d-','lj','nj','s=','z=']
-cnt = 0
-for al in cro:
-    for i in a:
-        if al==i:
-            cnt+=1
+# 처음엔 a, k 이런것도 알파벳 추가해야하는줄 .. 나 문해력 무슨일이냐
+# 그냥 len 하면 되는거 같음
+
+# cro = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+# word = input()
+# for i in cro:
+#     word = word.replace(i,'*')
+# print(len(word))
+
+# replace 쓰는거 신기하당 cnt해서 검정하려고 했는데
+
+
+# 1316 그룹 단어 체커
+# 진짜 모르겠다 시간좀 걸림
+
+n = int(input())
+result = 0
+for i in range(n):
+    word = input()
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            result += 1
+            break
+print(n-result)
+
+# 해냅니다 김가연이죠!!!!!!!!!!!!11
