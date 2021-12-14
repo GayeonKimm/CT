79ef6d75331ea014b13f1bb9d7fba5c5353e1854
y <gyk9704@gmail.com>
2021-12-14 17:25:19 +0900

    211214

---
+++ Programmers/BFS_DFS/타겟넘버.py
@@ -0,0 +1,47 @@
+# DFS
+
+# def solution(numbers, target):
+#     S = sum(numbers)
+#     L = [S]  # [5]
+#     print(len(L))
+#
+#     for n in numbers:
+#         Len = len(L)
+#         for idx in range(Len):
+#             L.append(L[idx]-2*n)
+#         print(L)
+#     return L.count(target)
+
+# DFS
+def dfs(idx, ans):
+    global answer, nb, tg
+    if idx == len(nb):
+        if ans == tg:
+            answer += 1
+        return
+    else:
+        dfs(idx+1, ans+nb[idx])
+        dfs(idx+1, ans-nb[idx])
+        return
+
+def solution(numbers, target):
+    global answer, nb, tg
+    nb = numbers
+    tg = target
+    answer = 0
+    dfs(0,0)
+    return answer
+
+# 재귀함수
+# def solution(numbers, target):
+#     if not numbers and target == 0:
+#         return 1
+#     elif not numbers:
+#         return 0
+#     else:
+#         return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+
+
+numbers = [1,1,1,1,1]
+target = 3
+print(solution(numbers, target))
--- Programmers/DP/N으로 표현.py
+++ Programmers/DP/N으로 표현.py
@@ -1,18 +1,12 @@
 # level 3
+# *****
+
 def solution(N, number):
     if N == number:
         return 1
-
     s = [set() for i in range(8)]
-    print(s)
-    print(len(s))
-
     for x in range(len(s)):
         s[x].add(int(str(N)*(x+1)))
-    print(s)
-
-    #### Check Code
-
     for i in range(1,len(s)):
         for j in range(i):
             for op1 in s[j]:
@@ -25,9 +19,7 @@ def solution(N, number):
         # print(s)
         if number in s[i]:
             return i+1
-
     return -1
-
 
 N = 5
 number = 12
