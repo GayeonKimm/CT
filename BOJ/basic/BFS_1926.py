ea0d8884b8dc540b3b618bcc50e3057a77b12e8d
y <gyk9704@gmail.com>
2021-12-15 14:00:38 +0900

    211215

--- BOJ/basic/BFS_1926.py
+++ BOJ/basic/BFS_1926.py
@@ -14,13 +14,12 @@ BFS
 - 방문 : bool[][] : 2차원 배열
 - Queue(BFS)
 """
-
 from collections import deque
 
 import sys
 input = sys.stdin.readline
 
-n,m = map (int, input().split())
+n,m = map(int, input().split())
 map = [list(map(int, input().split())) for _ in range(n)]
 chk = [[False] * m for _ in range(n)]
 
@@ -28,37 +27,35 @@ dy = [0,1,0,-1]
 dx = [1,0,-1,0]
 
 def bfs(y, x):
-    rs = 1   # 그림의 사이즈
+    rs = 1
     q = deque()
-    q.append((y,x))
+    q.append((y, x))
+    print("------------")
+    print("시작 q = ",q)
 
     while q:
         ey, ex = q.popleft()
         for k in range(4):
             ny = ey + dy[k]
             nx = ex + dx[k]
-
-            # 사이즈 넘어가지 않도록
             if 0<=ny<n and 0<=nx<m:
                 if map[ny][nx] == 1 and chk[ny][nx] == False:
                     rs += 1
                     chk[ny][nx] = True
-                    q.append((ny, nx))
+                    q.append((ny,nx))
+                    print("q = ", q)
+                    print("rs = ", rs)
     return rs
-# 전체 크기 리턴
 
-cnt = 0  # 전체
-maxv = 0  # maximum value
-
+cnt = 0
+maxv = 0
 for j in range(n):
     for i in range(m):
         if map[j][i] == 1 and chk[j][i] == False:
             chk[j][i] = True
-            # 그림갯수로 올리고
-            # bfs 를 통해 그림의 크기를 구해주기
             cnt += 1
+            # maxv = bfs(j, i)
             maxv = max(maxv, bfs(j,i))
-
 
 print(cnt)
 print(maxv)
