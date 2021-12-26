062510dfe090cd12313a8719eaf6b68293a11048
y <gyk9704@gmail.com>
2021-12-26 20:19:12 +0900

    211226

--- String_2/10610.py
+++ String_2/10610.py
@@ -1,22 +1,22 @@
 # 10610 30의 배수 찾기
 
-import sys
-input = sys.stdin.readline
-
-k = list(input().strip())
-k.sort(reverse=True)
-sum = 0
-for i in k:
-    sum += int(i)
-
-if sum % 3 == 0:
-    if k[-1] == '0':
-        print("".join(k))
-    else:
-        print('-1')
-
-else:
-    print('-1')
+# import sys
+# input = sys.stdin.readline
+#
+# k = list(input().strip())
+# k.sort(reverse=True)
+# sum = 0
+# for i in k:
+#     sum += int(i)
+#
+# if sum % 3 == 0:
+#     if k[-1] == '0':
+#         print("".join(k))
+#     else:
+#         print('-1')
+#
+# else:
+#     print('-1')
 
 # 이렇게 했을 때 k[-1] == '0' (0-> '0')으로 변경해야 돌아감
 # k 를 list로 받았을 때, 숫자로 받느냐 문자로 받느냐에 따라 달라짐
@@ -25,16 +25,20 @@ else:
 # print join을 할때도 문자만 합쳐질 수 있나봄
 #
 
-# # k = list(map(int, input().strip()))
-# k = list(input().strip())
-# # 숫자가 아니라 문자로 입력을 받았어야 했네
-# print(k)
-# k.sort(reverse=True)
-# sum = 0
-# for i in k:
-#     sum += int(i)
-# if sum % 3 != 0 or "0" not in k:
-#     print(-1)
-#
-# else:
-#     print("".join(k))
+# k = list(map(int, input().strip()))
+k = list(input().strip())
+# 숫자가 아니라 문자로 입력을 받았어야 했네
+
+k.sort(reverse=True)
+sum = 0
+
+for i in k:
+    sum += int(i)
+if sum % 3 != 0 or "0" not in k:
+    print(-1)
+
+else:
+    print("".join(k))
+
+
+
---
+++ String_2/11656.py
@@ -0,0 +1,16 @@
+# 11656 접미사 배열
+import sys
+input = sys.stdin.readline
+
+s = input().strip()
+answer = []
+for i in range(len(s)):
+    answer.append(s[i:])
+
+answer.sort()
+for i in answer:
+    print(i)
+
+
+
+
