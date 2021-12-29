43fcccc5622092afa78f9b770a7304cfc1c990af
y <gyk9704@gmail.com>
2021-12-29 15:25:54 +0900

    211229

--- String_2/9935.py
+++ String_2/9935.py
@@ -11,47 +11,50 @@
 #     print("FRULA")
 # else:
 #     print(word)
-# 틀렸대
+# 틀림
 
 # 스택으로 풀기
+# word와 pop을 리스트로 받음
+
+# import sys
+# input = sys.stdin.readline
+#
+# word = list(input().strip())
+# pop = list(input().strip())
+#
+# stack = []
+# for i in range(len(word)):
+#     stack.append(word[i])
+#     if stack[-1] == pop[-1] and len(stack) >= len(pop):
+#         if stack[-len(pop):] == pop:
+#             for i in range(len(pop)):
+#                 stack.pop()
+#
+# if stack:
+#     print("".join(stack))
+# else:
+#     print("FRULA")
+
+
+# 더 빠르게 내방식대로 재작성
+# word와 pop을 string으로 받음
+
 import sys
 input = sys.stdin.readline
 
-word = list(input().strip())
-pop = list(input().strip())
+word = input().strip()
+pop = input().strip()
+w = len(word)
+p = len(pop)
+lastChar = pop[-1]
 
 stack = []
-for i in range(len(word)):
-    stack.append(word[i])
-    if stack[-1] == pop[-1] and len(stack) >= len(pop):
-        if stack[-len(pop):] == pop:
-            for i in range(len(pop)):
-                stack.pop()
+for i in word:
+    stack.append(i)
+    if i == lastChar and "".join(stack[-p:]) == pop: # string으로 받았기 때문에 join 사용
+        del stack[-p:]
 
 if stack:
     print("".join(stack))
 else:
     print("FRULA")
-
-
-## 더 빠른 방법
-
-import sys
-input = sys.stdin.readline
-
-s = input().strip()
-p = input().strip()
-n = len(s)
-m = len(p)
-lastChar = p[-1]
-
-stack = []
-for char in s:
-    stack.append(char)
-    if char == lastChar and "".join(stack[-m:]) == p:
-        del stack[-m:]
-
-if len(stack) == 0:
-    print("FRULA")
-else:
-    print("".join(stack))
