3a64eb682277058fd2f0625ef4a300adb3750ffb
y <gyk9704@gmail.com>
2022-01-05 20:56:26 +0900

    220105

--- Programmers/1_level/신규 아이디 추천.py
+++ Programmers/1_level/신규 아이디 추천.py
@@ -1,25 +1,30 @@
 def solution(new_id):
-    answer = ''
+    #1
     new_id = new_id.lower()
-    if len(new_id) == 0:
-        new_id = 'a'
-        makeid(new_id)
 
+    #2
+    answer = ''
+    for i in new_id:
+        if i.isalnum() or i in '-_.':
+            answer += i
+    #3
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+    #4
+    answer = answer[1:] if answer[0]=='.' and len(answer)>1 else answer
+    answer = answer[:-1] if answer[-1]=='.' else answer
 
-def makeid(new_id):
-    if 3 <= len(new_id) <= 16:
-        for i in range(len(new_id)):
-            if new_id[i] in :
-
-
-    elif len(new_id) < 3:
-        new_id += new_id[-1] * (3 - len(new_id))
-        makeid(new_id)
-
-    elif len(new_id) > 16:
-        new_id = new_id[:15]
-        if new_id[-1] == '.':
-            new_id.remove(new_id[-1])
-            makeid(new_id)
-
+    if len(answer)==0:
+        answer = 'a'
+    #6
+    if len(answer) >= 16:
+        answer = answer[:15]
+        if answer[-1]=='.':
+            answer = answer[:-1]
+    #7
+    while len(answer) < 3 :
+        answer += answer[-1]
     return answer
+
+new_id = "...!@BaT#*..y.abcdefghijklm"
+print(solution(new_id))
