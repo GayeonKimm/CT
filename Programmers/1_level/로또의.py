ee28a84b36ce7946bf83be6117fc2670b2c9e29f
y <gyk9704@gmail.com>
2022-01-10 22:10:01 +0900

    220110

--- Programmers/1_level/로또의.py
+++ Programmers/1_level/로또의.py
@@ -2,16 +2,6 @@
 
 # def solution(lottos, win_nums):
 #     answer = []
-#     cnt = 7
-#     for i in lottos:
-#         if i == 0:
-#             cnt -= 1
-#         elif i in win_nums:
-#             cnt -= 1
-#     if cnt > 6:
-#         answer.append(6)
-#     else:
-#         answer.append(cnt)
 #
 #     cnt = 7
 #     for j in lottos:
