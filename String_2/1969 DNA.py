34c8a4f9d7a624270110187c1c6f68c750d0f363
y <gyk9704@gmail.com>
2022-01-19 10:11:06 +0900

    220119

--- String_2/1969 DNA.py
+++ String_2/1969 DNA.py
@@ -1,15 +1,41 @@
 # DNA
+# 코드 똑같이 작성했는데 틀렸다고함 이게 말이 되나
+
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 s = []
 for i in range(n):
-    s.append(input().strip())
+    s.append(list(input().strip()))
+# s.append(list(map(int, input().strip())))
 
-temp = [0 for _ in range(len(s))]
+hap = 0
+answer = ''
+for i in range(m):
+    a,c,g,t = 0,0,0,0
 
-for i in range(len(s)):
-    for j in range(len(s[0])):
-        temp[i] = s[i][j]
+    for j in range(n):
+        if s[j][i]=='T':
+            t += 1
+        elif s[j][i]=='A':
+            a += 1
+        elif s[j][i]=='G':
+            g += 1
+        elif s[j][i]=='C':
+            c += 1
+    if max(a,c,g,t) == a:
+        answer += 'A'
+        hap += c+g+t
+    elif max(a,c,g,t) == c:
+        answer += 'C'
+        hap += a+g+t
+    elif max(a,c,g,t) == g:
+        answer += 'G'
+        hap += c+a+t
+    elif max(a,c,g,t) == t:
+        answer += 'T'
+        hap += c+g+a
 
+print(answer)
+print(hap)
