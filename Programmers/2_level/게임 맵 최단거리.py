1c8c28d0a454f4699f187942e9075b307d21ea37
y <gyk9704@gmail.com>
2022-01-20 13:51:37 +0900

    220120

--- Programmers/2_level/게임 맵 최단거리.py
+++ Programmers/2_level/게임 맵 최단거리.py
@@ -1,6 +1,4 @@
 from collections import deque
-
-
 def solution(maps):
     r = len(maps)
     c = len(maps[0])
@@ -15,12 +13,10 @@ def solution(maps):
     dy = [0, 0, -1, 1]
     while q:
         x, y = q.popleft()
-
         for a in range(4):
             nx = x + dx[a]
             ny = y + dy[a]
             if 0 <= nx < r and 0 <= ny < c:
-                # 이 범위가 이상했어서 오류 났는데 이렇게 쓴게 맞음!
                 if maps[nx][ny]:
                     if table[nx][ny] == -1:
                         table[nx][ny] = table[x][y] + 1
@@ -34,9 +30,3 @@ print(solution([[1,0,1,1,1],
                 [1,0,1,1,1],
                 [1,1,1,0,1],
                 [0,0,0,0,1]]))
-
-# [[1, -1, 9, 10, 2],
-#  [2, -1, 8, -1, 3],
-#  [3, -1, 7, 8, 4],
-#  [4, 5, 6, -1, 5],
-# [-1, -1, -1, -1, 3]]
