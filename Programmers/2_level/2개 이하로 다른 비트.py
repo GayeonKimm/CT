55489c0387cfba475ccb788d68fd1e581b19498e
y <gyk9704@gmail.com>
2022-01-29 12:17:15 +0900

    220129

--- Programmers/2_level/2개 이하로 다른 비트.py
+++ Programmers/2_level/2개 이하로 다른 비트.py
@@ -14,14 +14,26 @@
 # def solution(numbers):
 #     answer = [fx(i) for i in numbers]
 #     return answer
-
 # 시간초과로 실패
 
 # method 2
-def solution(numbers):
-    answer = [fx(i) for i in numbers]
-    return answer
 
+def solution(numbers):
+    answer = []
+    for num in numbers:
+        bin_num = list('0' + bin(num)[2:]) # num을 이진수로 변경
+        idx = ''.join(bin_num).rfind('0') # 0이 있는 위치를 찾음
+        bin_num[idx] = '1' # 그 자리를 1로 변경
+        # 여기까지가 마지막 0을 1로 바꾼 과정 (짝수에게는 걍 num+1)
+
+        # 만약 홀수면
+        if num % 2 == 1:
+            bin_num[idx+1] = '0' # 1로 바꾼 그 자리 다음을 0으로 변경
+        # 이래야 제일 작은 수를 찾는대
+
+        # 2진수를 10진수로 변경하여 answer에 저장
+        answer.append(int(''.join(bin_num), 2))
+    return answer
 
 numbers = [2,7]
 print(solution(numbers))
---
+++ Programmers/2_level/[1차] 프렌즈4블록.py
@@ -0,0 +1,46 @@
+def solution(m,n,board):
+    answer = 0
+    for i in range(len(board)):
+        board[i] = list(board[i])
+
+    while True:
+        remove = [[0]*n for _ in range(m)]
+        # 블록 찾으면 remove 배열에 1로 표시
+        # 배열의 오른쪽과 하단 끝은 비교할 것이 없이 때문에 range 조절
+        for i in range(m-1):
+            for j in range(n-1):
+                if board[i][j] != 0 and board[i][j] == board[i][j+1] and board[i][j] == board[i+1][j] and board[i][j] == board[i+1][j+1]:
+                    remove[i][j], remove[i][j+1], remove[i+1][j], remove[i+1][j+1] = 1, 1, 1, 1
+
+        # ramove 개수 세기
+        count = 0
+        for i in range(m):
+            count += sum(remove[i])
+        answer += count
+
+        # 근데 지워질게 하나도 없다? 그럼 게임 while 종료
+        if count == 0:
+            break
+
+        # 블럭 채우기
+        for i in range(m-1, -1, -1):
+            for j in range(n):
+                # 리무브 값이 1이면 한칸 내려와야 하니까 x = i-1
+                if remove[i][j] == 1:
+                    x = i-1
+                    # 끝까지 내려가게
+                    while x >= 0 and remove[x][j] == 1:
+                        x -= 1
+                    # 다 내려왔으면 없으면 0 처리
+                    if x < 0:
+                        board[i][j] = 0
+                    # 위치변경
+                    else:
+                        board[i][j] = board[x][j]
+                        remove[x][j] = 1 # 여기 1 왜 하지?
+
+    return answer
+
+
+m,n,board = 4,5,["CCBDE", "AAADE", "AAABF", "CCBBF"]
+print(solution(m,n,board))
