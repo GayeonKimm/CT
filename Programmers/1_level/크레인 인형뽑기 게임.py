4f57c9eba1af8eb2ee02778e5e696e08a875fa74
y <gyk9704@gmail.com>
2022-01-31 19:03:23 +0900

    220131

--- Programmers/1_level/크레인 인형뽑기 게임.py
+++ Programmers/1_level/크레인 인형뽑기 게임.py
@@ -3,21 +3,40 @@
 # 터트릴거 answer에 추가
 # answer의 총 갯수를 반환
 
-def solution(board, moves):
-    answer = []
-    stack = []
-    for move in moves:
-        for i in range(len(board)):
-            if board[i][move-1] > 0:
-                stack.append(board[i][move-1])
-                board[i][move-1] = 0
+# def solution(board, moves):
+#     answer = []
+#     stack = []
+#     for move in moves:
+#         for i in range(len(board)):
+#             if board[i][move-1] > 0:
+#                 stack.append(board[i][move-1])
+#                 board[i][move-1] = 0
+#
+#                 if stack[-1:] == stack[-2:-1]:
+#                     answer.append(stack[-2:])
+#                     stack = stack[:-2]
+#                 break # 다 확인했으면 break해서 for문 넘어가야 됨!
+#     # print(answer)
+#     return len(answer)*2
 
-                if stack[-1:] == stack[-2:-1]:
-                    answer.append(stack[-2:])
-                    stack = stack[:-2]
-                break # 다 확인했으면 break해서 for문 넘어가야 됨!
-    # print(answer)
-    return len(answer)*2
+# 다른 풀이
+def solution(board, moves):
+    answer = 0
+    stack = []
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] > 0:
+                if not stack:
+                    stack.append(board[j][i-1])
+                else:
+                    if stack[-1] == board[j][i-1]:
+                        stack.pop()
+                        answer += 2
+                    else:
+                        stack.append(board[j][i-1])
+                board[j][i-1] = 0
+                break
+    return answer
 
 print(solution([[0,0,0,0,0],
                 [0,0,1,0,3],
