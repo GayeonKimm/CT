e2e14071fbd90c21f487e2b8321ef9a1958a9a2c
y <gyk9704@gmail.com>
2022-02-06 11:57:23 +0900

    220206

---
+++ z/200205 LGT/2.py
@@ -0,0 +1,11 @@
+def solution(maps):
+
+    return
+
+maps = [['A', 'B', 'T', 'T', 'T'],
+        ['T', 'C', 'D', 'E', 'T'],
+        ['T', 'T', 'T', 'F', 'T'],
+        ['B', 'A', 'H', 'G', 'T'],
+        ['C', 'D', 'E', 'F', 'G']]
+print(solution(maps))
+# 15
