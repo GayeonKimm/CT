b2aaa0b35dd55b01a84e742ed95b5699af55d92f
y <gyk9704@gmail.com>
2022-02-27 10:26:09 +0900

    220227

--- Programmers/2_level/124 나라의 숫자.py
+++ Programmers/2_level/124 나라의 숫자.py
@@ -1,9 +1,14 @@
+# 이건 몇번을 해도 모르겠음 진짜
+# +6
+
 def solution(n):
-    temp = [1,2,4]
+    temp = [1, 2, 4]
     answer = ''
-    while n>0:
+    while n > 0:
         n -= 1
-        answer = str(temp[n%3]) + answer
+        answer = str(temp[n % 3]) + answer
         n = n//3
     return answer
+
+
 print(solution(14))
