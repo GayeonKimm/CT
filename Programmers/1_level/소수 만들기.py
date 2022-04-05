b8dd41f670beea17cb0ead74d4a5d45eb7e4cd96
Gayeon <gyk9704@gmail.com>
2022-04-05 23:58:31 +0900

    220405

--- Programmers/1_level/소수 만들기.py
+++ Programmers/1_level/소수 만들기.py
@@ -3,20 +3,19 @@
 # 만든 숫자 리스트들을 for문 돌리면서 각 숫자들을 더하고
 # 더한 그 값이 소수인지 판별
 
+from itertools import combinations
 def solution(nums):
-    from itertools import combinations
     answer = 0
-
     temp = list(combinations(nums, 3))
     for i in temp:
         s = sum(i)
+
         for j in range(2, int(s**0.5)+1):
-            if s%j == 0: # 소수임
-                break # --> for i in temp:
-            #--> else:
+            if s % j == 0: # 소수 아님
+                break      # --> for i in temp: 로 돌아가
+        # break가 안되면 소수
         else:
             answer += 1
-
     return answer
 
 nums = [1,2,7,6,4]
