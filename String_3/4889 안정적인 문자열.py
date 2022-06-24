d54f7da5fcc29f5e2e754fe58fc3a6762fc3e9fa
GayeonKimm <gyk9704@gmail.com>
2022-06-24 20:34:06 +0900

    220624

--- String_3/4889 안정적인 문자열.py
+++ String_3/4889 안정적인 문자열.py
@@ -1,3 +1,22 @@
+'''
+문자열 열면 닫아야 해
+- 괄호 방향을 바꾸는 방법밖에 없음
+}가 들어오면 {로 바꾸자
+항상 길이가 짝수다???
+'''
+
 import sys
 input = sys.stdin.readline
+
+n = 0
+while True:
+    n += 1
+    s = input().strip()
+    answer = 0
+    if '-' in s:
+        break
+
+    for i in range(len(s)):
+
+    print(n,'.', answer)
 
