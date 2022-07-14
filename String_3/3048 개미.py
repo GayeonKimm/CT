9fe925a49518c8b4437492bb6f05c9ab7ded0a2c
GayeonKimm <gyk9704@gmail.com>
2022-07-14 12:36:06 +0900

    220714

--- String_3/3048 개미.py
+++ String_3/3048 개미.py
@@ -1,32 +1,28 @@
+'''
+처음에 헤매다가 해결방법 혼자 완성해나갔다
+아이캔두잇
+'''
+
 import sys
 input = sys.stdin.readline
 
 n1, n2 = map(int, input().split())
+a = list(map(str, input().strip()))
+a.reverse()
+b = list(map(str, input().strip()))
 
-road = []
-a = input().strip()
-for i in a:
-    road.insert(0, (i, '0'))
-b = input().strip()
-for i in b:
-    road.append((i, '-1'))
+road = a + b
+# print(road)
 
-t = int(input())
-print(road)
-
-# 이제 비교하자 자리 바꾸는 과정
-for _ in range(t):
-    # print(road)
+# 자리 바꾸는 과정
+for _ in range(int(input())):
     for i in range(len(road)-1):
-        if road[i][1] == '0' and road[i+1][1] == '-1':
+        if road[i] in a and road[i+1] in b:
             road[i], road[i+1] = road[i+1], road[i]
-
-            # 이게 계속 이동함 걍 미친듯이 이동
-            # 선두 개미이면 반복을 멈춘다는게 뭔소리지?
-            if road[i+1][0] == :
+            if road[i+1] in a[-1]:
                 break
 
 answer = ''
-for i in range(len(road)):
-    answer += road[i][0]
+for i in road:
+    answer += i
 print(answer)
