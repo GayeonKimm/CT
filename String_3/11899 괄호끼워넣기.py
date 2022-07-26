b7e208a85417b11c37130309b00799dd4cf44dbd
GayeonKimm <gyk9704@gmail.com>
2022-07-26 21:41:43 +0900

    220724

--- String_3/11899 괄호끼워넣기.py
+++ String_3/11899 괄호끼워넣기.py
@@ -11,9 +11,8 @@ stack = []
 for i in range(len(s)):
     if s[i] == '(':
         stack.append(s[i])
-
     else:
-        if stack and stack[-1] =='(':
+        if stack and stack[-1] == '(':
             stack.pop()
         else:
             stack.append(s[i])
